8dc94611a1719e82dec9c6d68b5804ece8704651
rediska <sherstnev.denis.v@gmail.com>
2024-10-11 22:03:09 +0300

    design fixes

--- website/src/components/Stats/Stats.tsx
+++ website/src/components/Stats/Stats.tsx
@@ -16,27 +16,26 @@ export default function Stats(props: IStatsProps) {
   return (
     <section className={styles.container}>
         <section className={styles.landing}>
-          <h1 className={styles.header}>Покерная лига 🃏</h1>
+          <h1 className={styles.header}>🃏 Покерная лига</h1>
           <h3 className={styles.subheader}><img src={coolSmile} alt="" width={30} height={30} /> <span>Играют только профессионалы</span></h3>
-          <div className={styles.buttons}>
-            <a href='mailto:sherstnev.denis.v@gmail.com' target='_blank' className={styles.button + ' ' + styles.contact}>
-              <img src={emailIcon} alt="" width={20} />
-              <span>Написать нам</span>
-            </a>
-          </div>
           <h2 className={styles.stats_header}>
             <CountUp className={styles.accent_number} end={props.stats.total_games} /> игр сыграно
           </h2>
           <h2 className={styles.stats_header}>
             <CountUp className={styles.accent_number} end={props.stats.total_chips} /> фишек в банке
           </h2>
+          <div className={styles.buttons}>
+            <a href='mailto:sherstnev.denis.v@gmail.com' target='_blank' className={styles.button + ' ' + styles.contact}>
+              <img src={emailIcon} alt="" width={20} />
+              <span>Написать нам</span>
+            </a>
+          </div>
         </section>
         <section className={styles.stats}>
           <div className={styles.chart}>
             <div className={styles.chart_header}>Распределение побед</div>
             <PieChart
-              width={500}
-              height={300}
+              height={400}
               slotProps={{
                 legend: {
                   labelStyle: {
